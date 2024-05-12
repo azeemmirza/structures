28e9a5360e3e6c8302998b6191147ee67b25e32c
Azeem Mirza <azeem.mirza94@gmail.com>
2024-05-12 17:10:53 +0500

    chore: updated tsconfig, eslint rc and removed test files

--- src/utils/test-utils.ts
+++
@@ -1,7 +0,0 @@
-export function sum(...args: number[]) {
-  return args.reduce((acc, cur) => acc + cur, 0);
-}
-
-export function subtract(...args: number[]) {}
-
-export function multiply(...args: number[]) {}
